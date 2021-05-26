45ba1a184d9c143ec1ed075da0830f5c9a1378e3
adityaJayamSingh <82505795+adityaJayamSingh@users.noreply.github.com>
2021-05-26 20:46:21 +0530

    Create Sum of Two Values.cpp

---
+++ Sorting and Searching/Sum of Two Values.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+#include<algorithm>
+#include<bitset>
+#include<cmath>
+#include<set>
+#include<map>
+ 
+using namespace std;
+ 
+#define MOD 1000000007 
+ 
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	
+	long n,sum;
+	cin>>n>>sum;
+	vector<long>a(n);
+	map<long,long> mp;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	mp[a[0]]=0;
+	for(int i=1;i< n;i++)
+	{
+		if(mp.count(sum-a[i])> 0)
+		{
+			cout<<mp[sum-a[i]]+1<<' '<<i+1;
+			return 0;
+		}
+		else
+		{
+			mp[a[i]]=i;
+		}
+		
+	}
+	cout<<"IMPOSSIBLE";
+	return 0;
+}
